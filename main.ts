fe89a7a1a7061df7ce1467c4dc72bda53ae338bf
kong <duguyihou@gmail.com>
2023-03-06 08:56:22 +1100

    fix: cors

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
+import { oakCors } from "./dep.ts";
 import { Application } from "./dep.ts";
 import { routers } from "./routes/routes.ts";
 
 const app = new Application();
 
+app.use(oakCors())
 app.use(routers);
 
 await app.listen({ port: 8000 });
