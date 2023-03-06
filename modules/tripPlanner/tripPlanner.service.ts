8e1dcce67d42c46823e70e335c13676bb557561d
kong <duguyihou@gmail.com>
2023-03-06 19:47:56 +1100

    fix: wip

--- modules/tripPlanner/tripPlanner.service.ts
+++ modules/tripPlanner/tripPlanner.service.ts
@@ -2,6 +2,7 @@ import configService from "../../config/configService.ts";
 
 const stopFinder = async (name_sf: string) => {
   const { baseUrl, apiKey } = configService;
+  console.log("🐵  ------ baseUrl", baseUrl);
   const params = [
     `outputFormat=rapidJSON`,
     `type_sf=any`,
@@ -12,6 +13,7 @@ const stopFinder = async (name_sf: string) => {
   ].join("&");
 
   const url = `${baseUrl}/v1/tp/stop_finder?${params}`;
+  console.log("🐵  url------ ", url);
 
   const sfResponse = await fetch(url, {
     headers: {
