74276f968a57ee284a33e91eb07c389fde5cb8e1
kong <duguyihou@gmail.com>
2023-03-20 21:14:27 +1100

    chore: remove env path

--- config/configService.ts
+++ config/configService.ts
@@ -1,9 +1,10 @@
 import { dotenv } from "../dep.ts";
 
-const config = await dotenv.load({export:true, envPath:'../.env'});
+const config = await dotenv.load({ export: true });
 
 const configService = {
   baseUrl: config["BASE_URL"],
   apiKey: config["API_KEY"],
 };
+console.log("🐵  ------config ", config);
 export default configService;
