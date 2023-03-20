402fd00c87867fb87d0ddd158816082aff379b1b
kong <duguyihou@gmail.com>
2023-03-20 21:18:55 +1100

    chore: test

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-import { oakCors } from "./dep.ts";
+import { dotenv, oakCors } from "./dep.ts";
 import { Application } from "./dep.ts";
 import { routers } from "./routes/routes.ts";
+import {} from "./dep.ts";
+
+await dotenv.load({ export: true });
 
 const app = new Application();
 
